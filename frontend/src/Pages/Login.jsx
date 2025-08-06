c05234bb35c81fb629c43ed4779cd04223446619
meghana <meghananijanapuram@gmail.com>
2025-08-06 19:06:42 +0530

    Updated Login.jsx file

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/login",
+        `${BACKEND_URL}`,
         {
           ...inputValue,
         },
